4479ee64e616b37c38a6502c918d1807b78037f5
m-umarkhan999 <m.umarkhan999@gmail.com>
2017-07-25 08:18:49 +0500

    some gui changes

--- EadWebProject/Controllers/LikesController.cs
+++ EadWebProject/Controllers/LikesController.cs
@@ -14,114 +14,127 @@
 //    {
 //        private EadWebProjectContext db = new EadWebProjectContext();
 
-//        // GET: Likes
-//        public ActionResult Index()
-//        {
-//            return View(db.Likes.ToList());
-//        }
-
-//        // GET: Likes/Details/5
-//        public ActionResult Details(int? id)
-//        {
-//            if (id == null)
-//            {
-//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-//            }
-//            Like like = db.Likes.Find(id);
-//            if (like == null)
-//            {
-//                return HttpNotFound();
-//            }
-//            return View(like);
-//        }
-
-//        // GET: Likes/Create
-//        public ActionResult Create()
-//        {
-//            return View();
-//        }
-
-//        // POST: Likes/Create
-//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
 //        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public ActionResult Create([Bind(Include = "LikeID,UserID,ParentID,Status")] Like like)
+//        public String Like()
 //        {
-//            if (ModelState.IsValid)
+//            if(Session["login"] == null)
 //            {
-//                db.Likes.Add(like);
-//                db.SaveChanges();
-//                return RedirectToAction("Index");
+//                return null;
 //            }
+//            String userId = Session["login"].ToString();
+//            User user = db.Users.Where(x => x.RoleID.Equals("Parent") && x.UserID.Equals(userId)).First();
+//            if (user == null)
+//                return null;
 
-//            return View(like);
 //        }
+//        //// GET: Likes
+//        //public ActionResult Index()
+//        //{
+//        //    return View(db.Likes.ToList());
+//        //}
 
-//        // GET: Likes/Edit/5
-//        public ActionResult Edit(int? id)
-//        {
-//            if (id == null)
-//            {
-//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-//            }
-//            Like like = db.Likes.Find(id);
-//            if (like == null)
-//            {
-//                return HttpNotFound();
-//            }
-//            return View(like);
-//        }
+//        //// GET: Likes/Details/5
+//        //public ActionResult Details(int? id)
+//        //{
+//        //    if (id == null)
+//        //    {
+//        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//        //    }
+//        //    Like like = db.Likes.Find(id);
+//        //    if (like == null)
+//        //    {
+//        //        return HttpNotFound();
+//        //    }
+//        //    return View(like);
+//        //}
 
-//        // POST: Likes/Edit/5
-//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public ActionResult Edit([Bind(Include = "LikeID,UserID,ParentID,Status")] Like like)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                db.Entry(like).State = EntityState.Modified;
-//                db.SaveChanges();
-//                return RedirectToAction("Index");
-//            }
-//            return View(like);
-//        }
+//        //// GET: Likes/Create
+//        //public ActionResult Create()
+//        //{
+//        //    return View();
+//        //}
 
-//        // GET: Likes/Delete/5
-//        public ActionResult Delete(int? id)
-//        {
-//            if (id == null)
-//            {
-//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-//            }
-//            Like like = db.Likes.Find(id);
-//            if (like == null)
-//            {
-//                return HttpNotFound();
-//            }
-//            return View(like);
-//        }
+//        //// POST: Likes/Create
+//        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+//        //[HttpPost]
+//        //[ValidateAntiForgeryToken]
+//        //public ActionResult Create([Bind(Include = "LikeID,UserID,ParentID,Status")] Like like)
+//        //{
+//        //    if (ModelState.IsValid)
+//        //    {
+//        //        db.Likes.Add(like);
+//        //        db.SaveChanges();
+//        //        return RedirectToAction("Index");
+//        //    }
 
-//        // POST: Likes/Delete/5
-//        [HttpPost, ActionName("Delete")]
-//        [ValidateAntiForgeryToken]
-//        public ActionResult DeleteConfirmed(int id)
-//        {
-//            Like like = db.Likes.Find(id);
-//            db.Likes.Remove(like);
-//            db.SaveChanges();
-//            return RedirectToAction("Index");
-//        }
+//        //    return View(like);
+//        //}
 
-//        protected override void Dispose(bool disposing)
-//        {
-//            if (disposing)
-//            {
-//                db.Dispose();
-//            }
-//            base.Dispose(disposing);
-//        }
+//        //// GET: Likes/Edit/5
+//        //public ActionResult Edit(int? id)
+//        //{
+//        //    if (id == null)
+//        //    {
+//        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//        //    }
+//        //    Like like = db.Likes.Find(id);
+//        //    if (like == null)
+//        //    {
+//        //        return HttpNotFound();
+//        //    }
+//        //    return View(like);
+//        //}
+
+//        //// POST: Likes/Edit/5
+//        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+//        //[HttpPost]
+//        //[ValidateAntiForgeryToken]
+//        //public ActionResult Edit([Bind(Include = "LikeID,UserID,ParentID,Status")] Like like)
+//        //{
+//        //    if (ModelState.IsValid)
+//        //    {
+//        //        db.Entry(like).State = EntityState.Modified;
+//        //        db.SaveChanges();
+//        //        return RedirectToAction("Index");
+//        //    }
+//        //    return View(like);
+//        //}
+
+//        //// GET: Likes/Delete/5
+//        //public ActionResult Delete(int? id)
+//        //{
+//        //    if (id == null)
+//        //    {
+//        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//        //    }
+//        //    Like like = db.Likes.Find(id);
+//        //    if (like == null)
+//        //    {
+//        //        return HttpNotFound();
+//        //    }
+//        //    return View(like);
+//        //}
+
+//        //// POST: Likes/Delete/5
+//        //[HttpPost, ActionName("Delete")]
+//        //[ValidateAntiForgeryToken]
+//        //public ActionResult DeleteConfirmed(int id)
+//        //{
+//        //    Like like = db.Likes.Find(id);
+//        //    db.Likes.Remove(like);
+//        //    db.SaveChanges();
+//        //    return RedirectToAction("Index");
+//        //}
+
+//        //protected override void Dispose(bool disposing)
+//        //{
+//        //    if (disposing)
+//        //    {
+//        //        db.Dispose();
+//        //    }
+//        //    base.Dispose(disposing);
+//        //}
 //    }
 //}
